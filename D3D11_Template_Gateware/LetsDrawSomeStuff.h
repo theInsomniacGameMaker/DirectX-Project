1d1a9a746ce1c2d923bb2d3759907d6ed8a0c7fd
Siddharth Jain <sjain@student.fullsail.edu>
2018-09-25 16:56:03 -0400

    Intial Project Setup

---
+++ D3D11_Template_Gateware/LetsDrawSomeStuff.h
@@ -0,0 +1,101 @@
+// This file can be used to contain very basic DX11 Loading, Drawing & Clean Up. (Start Here, or Make your own set of classes)
+#pragma once
+// Include our DX11 middle ware
+#include "Gateware Redistribution R5d/Interface/G_Graphics/GDirectX11Surface.h"
+
+// Include DirectX11 for interface access
+#include <d3d11.h>
+
+// Simple Container class to make life easier/cleaner
+class LetsDrawSomeStuff
+{
+	// variables here
+	GW::GRAPHICS::GDirectX11Surface* mySurface = nullptr;
+	// Gettting these handles from GDirectX11Surface will increase their internal refrence counts, be sure to "Release()" them when done!
+	ID3D11Device *myDevice = nullptr;
+	IDXGISwapChain *mySwapChain = nullptr;
+	ID3D11DeviceContext *myContext = nullptr;
+
+	// TODO: Add your own D3D11 variables here (be sure to "Release()" them when done!)
+
+public:
+	// Init
+	LetsDrawSomeStuff(GW::SYSTEM::GWindow* attatchPoint);
+	// Shutdown
+	~LetsDrawSomeStuff();
+	// Draw
+	void Render();
+};
+
+// Init
+LetsDrawSomeStuff::LetsDrawSomeStuff(GW::SYSTEM::GWindow* attatchPoint)
+{
+	if (attatchPoint) // valid window?
+	{
+		// Create surface, will auto attatch to GWindow
+		if (G_SUCCESS(GW::GRAPHICS::CreateGDirectX11Surface(attatchPoint, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT, &mySurface)))
+		{
+			// Grab handles to all DX11 base interfaces
+			mySurface->GetDevice((void**)&myDevice);
+			mySurface->GetSwapchain((void**)&mySwapChain);
+			mySurface->GetContext((void**)&myContext);
+
+			// TODO: Create new DirectX stuff here! (Buffers, Shaders, Layouts, Views, Textures, etc...)
+		}
+	}
+}
+
+// Shutdown
+LetsDrawSomeStuff::~LetsDrawSomeStuff()
+{
+	// Release DX Objects aquired from the surface
+	myDevice->Release();
+	mySwapChain->Release();
+	myContext->Release();
+
+	// TODO: "Release()" more stuff here!
+
+
+	if (mySurface) // Free Gateware Interface
+	{
+		mySurface->DecrementCount(); // reduce internal count (will auto delete on Zero)
+		mySurface = nullptr; // the safest way to fly
+	}
+}
+
+// Draw
+void LetsDrawSomeStuff::Render()
+{
+	if (mySurface) // valid?
+	{
+		// this could be changed during resolution edits, get it every frame
+		ID3D11RenderTargetView *myRenderTargetView = nullptr;
+		ID3D11DepthStencilView *myDepthStencilView = nullptr;
+		if (G_SUCCESS(mySurface->GetRenderTarget((void**)&myRenderTargetView)))
+		{
+			// Grab the Z Buffer if one was requested
+			if (G_SUCCESS(mySurface->GetDepthStencilView((void**)&myDepthStencilView)))
+			{
+				myContext->ClearDepthStencilView(myDepthStencilView, D3D11_CLEAR_DEPTH, 1, 0); // clear it to Z exponential Far.
+				myDepthStencilView->Release();
+			}
+
+			// Set active target for drawing, all array based D3D11 functions should use a syntax similar to below
+			ID3D11RenderTargetView* const targets[] = { myRenderTargetView };
+			myContext->OMSetRenderTargets(1, targets, myDepthStencilView);
+
+			// Clear the screen to dark green
+			const float d_green[] = { 0, 0.5f, 0, 1 };
+			myContext->ClearRenderTargetView(myRenderTargetView, d_green);
+			
+			// TODO: Set your shaders, Update & Set your constant buffers, Attatch your vertex & index buffers, Set your InputLayout & Topology & Draw!
+
+			// Present Backbuffer using Swapchain object
+			// Framerate is currently unlocked, we suggest "MSI Afterburner" to track your current FPS and memory usage.
+			mySwapChain->Present(0, 0); // set first argument to 1 to enable vertical refresh sync with display
+
+			// Free any temp DX handles aquired this frame
+			myRenderTargetView->Release();
+		}
+	}
+}
