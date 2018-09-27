1889f07219e02c3f6da1e400d4874ed704689474
Siddharth Jain <sjain@student.fullsail.edu>
2018-09-27 15:36:45 -0400

    FBX Loading and Shaders
    FBX loading implemenetd using the right DLLs and Library and shaders made using the .csh method.

--- D3D11_Template_Gateware/Declarations.h
+++ D3D11_Template_Gateware/Declarations.h
@@ -1,10 +1,37 @@
 #pragma once
 #include <windows.h>
-#include <d3d11.h>
+#include <d3d11_1.h>
 #include <d3dcompiler.h>
 #include <directxmath.h>
 #include <directxcolors.h>
 #include <fbxsdk.h>
 #include <iostream>
-using namespace std;
 using namespace DirectX;
+using namespace std;
+
+#include "Trivial_VS.csh"
+#include "Trivial_PS.csh"
+
+struct SimpleVertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+
+struct ConstantBuffer
+{
+	XMMATRIX mWorld;
+	XMMATRIX mView;
+	XMMATRIX mProjection;
+};
+
+// Arrays and counts for
+// FBX to DirectX data transfer
+SimpleVertex *vertices;
+int numVertices = 0;
+int *indices;
+int numIndices = 0;
+
+// funtime random color
+#define RAND_COLOR XMFLOAT4(rand()/float(RAND_MAX),rand()/float(RAND_MAX),rand()/float(RAND_MAX),1.0f)
