23b5def7d2800a1c0c86c58b1e94c6724f5558d2
Unknown <sjain@student.fullsail.edu>
2018-10-06 01:07:34 -0400

    Added New VS

---
+++ D3D11_Template_Gateware/VS_PositionModifier.hlsl
@@ -0,0 +1,44 @@
+
+
+cbuffer ConstantBuffer : register(b0)
+{
+	matrix World;
+	matrix View;
+	matrix Projection;
+	float4 time;
+	float4 vOutputColor;
+}
+
+cbuffer ConstantBuffer : register (b1)
+{
+	matrix worldArray[10];
+}
+
+struct VS_INPUT
+{
+	float4 Pos : POSITION;
+	float3 Norm : NORMAL;
+	float2 Tex : TEXCOORD0;
+	float4 Tan: TANGENT;
+};
+
+//--------------------------------------------------------------------------------------
+struct VS_OUTPUT
+{
+	float4 Pos : SV_POSITION;
+	float3 Norm : NORMAL;
+	float2 Tex : TEXCOORD1;
+	float4 wPos : POSITION;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output = (VS_OUTPUT)0;
+	output.Pos = mul(sin(time.x), input.Pos);
+	output.wPos = output.Pos;
+	output.Pos = mul(output.Pos, View);
+	output.Pos = mul(output.Pos, Projection);
+	output.Norm = mul(float4(input.Norm, 0), World).xyz;
+	output.Tex = input.Tex;
+	return output;
+}
