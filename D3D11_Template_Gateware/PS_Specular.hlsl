6823cca87029ff66e458e60753edf7f64485beb6
Unknown <sjain@student.fullsail.edu>
2018-10-15 07:04:15 -0400

    Specular Shader Changed
    Now specular is calculated for each  light and not overall because this would cause a shine when the light was not even present.

--- D3D11_Template_Gateware/PS_Specular.hlsl
+++ D3D11_Template_Gateware/PS_Specular.hlsl
@@ -36,7 +36,7 @@ float4 main(PS_INPUT input) : SV_Target
 	float4 finalColor = 0;
 	float4 pointLightColor = 0;
 	float4 directionalLigthColor = 0;
- 	float4 baseTexture = txDiffuse.Sample(samLinear, input.Tex);
+	float4 baseTexture = txDiffuse.Sample(samLinear, input.Tex);
 
 	if (all(baseTexture == float4(0, 0, 0, 0)))
 	{
@@ -54,40 +54,60 @@ float4 main(PS_INPUT input) : SV_Target
 		else if (lights[i].Position.w == 1)
 		{
 			ligthDir = (float3)lights[i].Direction;
-			finalColor += saturate(dot((float3)lights[i].Direction, input.Norm) * lights[i].Color * baseTexture);
+
+			float4 directionalLight = saturate(dot((float3)lights[i].Direction, input.Norm) * lights[i].Color * baseTexture);
+
+			float4 viewDir = normalize(float4(View._m03, View._m13, View._m23, View._m33) - input.wPos);
+			float3 halfVector = normalize(ligthDir + viewDir);
+			float intensity = max(pow(saturate(dot(input.Norm, halfVector)), 32), 0);
+			float4 reflectedLight = directionalLight * intensity;
+
+			finalColor += saturate(reflectedLight * directionalLight);
 		}
 		else if (lights[i].Position.w == 2)
 		{
 			float3 lightToPixelVec = lights[i].Position - input.wPos;
-			
+
 			ligthDir = lightToPixelVec;
-			
+
 			float howMuchLight = saturate(dot(normalize(lightToPixelVec), input.Norm));
 			pointLightColor = howMuchLight * baseTexture* lights[i].Color;
 			pointLightColor *= (1.0 - saturate(length(lights[i].Position - input.wPos) / lights[i].Range.x));
-			finalColor += saturate(pointLightColor);
+
+			float4 viewDir = normalize(float4(View._m03, View._m13, View._m23, View._m33) - input.wPos);
+			float3 halfVector = normalize(ligthDir + viewDir);
+			float intensity = max(pow(saturate(dot(input.Norm, halfVector)), 32), 0);
+			float4 reflectedLight = pointLightColor * intensity;
+
+			finalColor += saturate(pointLightColor*+ reflectedLight);
 		}
 		else if (lights[i].Position.w == 3)
 		{
 			float3 lightToPixelVec = normalize(lights[i].Position - input.wPos);
 
 			ligthDir = lightToPixelVec;
-			
+
 			float surfaceRatio = saturate(dot(-lightToPixelVec, float3(lights[i].Direction.x, lights[i].Direction.y, lights[i].Direction.z)));
 			float spotFactor = (surfaceRatio > lights[i].Range.y) ? 1 : 0;
 			float lightRatio = saturate(dot(lightToPixelVec, input.Norm));
-			float3 spotLightColor = spotFactor*lightRatio*lights[i].Color*baseTexture;
+			float3 spotLightColor = spotFactor * lightRatio*lights[i].Color*baseTexture;
 			spotLightColor *= (1.0 - saturate(length(lights[i].Position - input.wPos) / lights[i].Range.z));
 			spotLightColor *= (1.0 - saturate((lights[i].Range.x - surfaceRatio) / (lights[i].Range.x - lights[i].Range.y)));
-			finalColor += float4(spotLightColor,1);
+
+			float4 viewDir = normalize(float4(View._m03, View._m13, View._m23, View._m33) - input.wPos);
+			float3 halfVector = normalize(ligthDir + viewDir);
+			float intensity = max(pow(saturate(dot(input.Norm, halfVector)), 8), 0);
+			float3 reflectedLight = spotLightColor * intensity;
+
+			finalColor += (float4(spotLightColor,1) * float4(reflectedLight,1));
 		}
 
-		float4 viewDir = normalize(float4(View._m03, View._m13, View._m23, View._m33) - input.wPos);
+		/*float4 viewDir = normalize(float4(View._m03, View._m13, View._m23, View._m33) - input.wPos);
 		float3 halfVector = normalize(ligthDir + viewDir);
 		float intensity = max(pow(saturate(dot(input.Norm, halfVector)), 32),0);
 		float4 reflectedLight = lights[i].Color*intensity;
-		finalColor += reflectedLight;
-	}	
+		finalColor += reflectedLight;*/
+	}
 
 
 
